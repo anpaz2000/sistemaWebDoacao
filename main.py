bc15dd2494ce70b357ca3f32f968baa0718193ac
anpaz2000 <anpaz2000@gmail.com>
2024-04-25 19:47:30 -0300

    Rota cadastro e ajuste dos links

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=105, debug=True)
+
+    
