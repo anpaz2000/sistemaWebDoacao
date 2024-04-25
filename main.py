bde1d8087cd5983e41c00d75afd1664c40cb7abf
anpaz2000 <anpaz2000@gmail.com>
2024-04-25 19:24:25 -0300

    Primeira versão pagina login e cadastro

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import sqlalchemy
-print("Olá")
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("home.html")
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=105, debug=True)
